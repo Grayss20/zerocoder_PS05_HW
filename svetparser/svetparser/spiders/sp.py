f485d7e786e2b9c71213edb2f83a0c38f066025b
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-08-22 18:57:47 +0100

    final

---
+++ svetparser/svetparser/spiders/sp.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class SpSpider(scrapy.Spider):
+    name = "sp"
+    allowed_domains = ["https://www.divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        svets = response.css("div.wYUX2")
+        for svet in svets:
+            yield {
+                "name": svet.css("a span::text").get(),
+                "price": svet.css("span.ui-LD-ZU.KIkOH::text").get(),
+                "url": svet.css("a::attr(href)").get()  # divan.css("a").attrib['href']
+            }
